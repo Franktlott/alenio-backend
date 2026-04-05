9a9f2f0059d11c1de09baa97baf0893c94be06fb
Sandbox user <tools@notanumber.com>
2026-04-05 23:43:20 +0000

    Please fix the following errors: Unknown named module: "expo-network"  codeFrame:    22 |     const moduleId = verboseNamesToModuleIds.get(verboseName);   23 |     if (moduleId == null) { > 24 |      ...

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import "expo-network";
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack, router } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
